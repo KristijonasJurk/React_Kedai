a29bf27f0af43e09cf47ef2590c2703b1c89549f
KristijonasJurk <70882614+KristijonasJurk@users.noreply.github.com>
2021-02-25 21:55:57 +0200

    logos, styles, navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,30 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import logo from '../img/logo/logo.png'
 
 const Navbar = () => {
     return (
         <nav className='navbar'>
             <div className="nav-center">
                 <Link to='/'>
+                    <img src={logo} alt="logo" className='logo' />
                 </Link>
                 <ul className="nav-links">
+                    <li>
+                        <Link to='/womens'>
+                            Women's
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to='/mens'>
+                            Men's
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to='/accessories'>
+                            Accessories &#38; Lifestyle
+                        </Link>
+                    </li>
                     <li>
                         <Link to='/'>
                             Home
