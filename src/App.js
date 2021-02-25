5d80573e36c1b2e4704cc1615a3dcff6c25e21a4
KristijonasJurk <70882614+KristijonasJurk@users.noreply.github.com>
2021-02-25 16:54:44 +0200

    navbar, error, home pages and react routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+// import pages
+import Home from './pages/Home'
+import Navbar from './components/Navbar'
+import Error from './components/Error'
+import Loading from './components/Loading'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>helllooo</p>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/loading" component={Loading} />
+          <Route path="*" component={Error} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+    return (
+        <nav className='navbar'>
+            <div className="nav-center">
+                <Link to='/'>
+                </Link>
+                <ul className="nav-links">
+                    <li>
+                        <Link to='/'>
+                            Home
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
