bce4b84e87160a4f8e7174ec076815c7cd96247c
KristijonasJurk <70882614+KristijonasJurk@users.noreply.github.com>
2021-02-25 23:09:42 +0200

    adding styles of navbar, react icons, font

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,36 +1,40 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import logo from '../img/logo/logo.png'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../img/logo/logo.png';
+import { IoBagOutline } from 'react-icons/io5';
 
 const Navbar = () => {
     return (
         <nav className='navbar'>
             <div className="nav-center">
-                <Link to='/'>
+                <Link to='/' className='nav-link'>
                     <img src={logo} alt="logo" className='logo' />
                 </Link>
                 <ul className="nav-links">
                     <li>
-                        <Link to='/womens'>
+                        <Link to='/womens' className='nav-link'>
                             Women's
                         </Link>
                     </li>
                     <li>
-                        <Link to='/mens'>
+                        <Link to='/mens' className='nav-link'>
                             Men's
                         </Link>
                     </li>
                     <li>
-                        <Link to='/accessories'>
+                        <Link to='/accessories' className='nav-link'>
                             Accessories &#38; Lifestyle
                         </Link>
                     </li>
                     <li>
-                        <Link to='/'>
+                        <Link to='/' className='nav-link'>
                             Home
                         </Link>
                     </li>
                 </ul>
+                <div>
+                    <IoBagOutline />
+                </div>
             </div>
         </nav>
     )
