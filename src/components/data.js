c41121b576eaf44f4f6867c115dd6191d6e317da
KristijonasJurk <70882614+KristijonasJurk@users.noreply.github.com>
2021-03-03 00:39:11 +0200

    finally finished css grid, looks smooth

--- src/components/data.js
+++ src/components/data.js
@@ -5,14 +5,16 @@ export const navLinks = [
     { link: '/', text: `Home` }
 ]
 
-export const mainGrid = [
+export const mainGrid1 = [
     { src: 'https://images.ctfassets.net/q602vtcuu3w3/1Mgf8LBYI0r7MfVpJE57dK/bf5dee236028ae3823d2d43db8a968df/21_FEB_WK4_MYSTERY_PROMO_EM10.jpg?w=2160&q=80&fm=webp' },
-    { src: 'https://images.ctfassets.net/q602vtcuu3w3/78RQJxUu6Z97F5ZCCxUFdM/afac4a9f923374d3b07901749308497e/210301-WK1-MGW-1.jpg?q=80&fm=webp', title: 'OUTDOORS BRAND EDIT', text: 'Gear up for your next outdoor adventure in ready-for-anything apparel and accessories from your favorite brands. Shop' },
-    { src: 'https://images.ctfassets.net/q602vtcuu3w3/37TpxZl5QCMmmXdarLY63i/b901705f22025d0a86d6b202cf59911b/210301-WK1-MGW-2.jpg?q=80&fm=webp', title: 'NEW SHOES: ACTIVE EDITION', text: 'Trail sneakers, running shoes and sandals for your next day trip. Shop Shoes' },
+    { src: 'https://images.ctfassets.net/q602vtcuu3w3/78RQJxUu6Z97F5ZCCxUFdM/afac4a9f923374d3b07901749308497e/210301-WK1-MGW-1.jpg?q=80&fm=webp', title: 'OUTDOORS BRAND EDIT', text: 'Gear up for your next outdoor adventure in ready-for-anything apparel and accessories from your favorite brands.  merch and more' },
+    { src: 'https://images.ctfassets.net/q602vtcuu3w3/37TpxZl5QCMmmXdarLY63i/b901705f22025d0a86d6b202cf59911b/210301-WK1-MGW-2.jpg?q=80&fm=webp', title: 'NEW SHOES: ACTIVE EDITION', text: 'Trail sneakers, running shoes and sandals for your next day.' },
     { src: 'https://images.ctfassets.net/q602vtcuu3w3/4fv2QUZ8DQgHFS4NfX2u4S/17dabf874daa8cd553033984cd3470b4/210301-WK1-HP-3.jpg?q=80&fm=webp', title: 'NEW IN THE GRAPHICS SHOP', text: 'All-new music graphics ft. your favorite artists. Shop Graphics' },
-    { src: 'https://images.ctfassets.net/q602vtcuu3w3/1RZY1sC9SRpxYc7GWifbML/6452f71c115387bcf4036384e1ecc460/210301-WK1-HP-4.jpg?q=80&fm=webp', title: 'NEW FURNITURE: UPSTATE MODERN', text: 'Bright wood furniture, soft, modern textures, classic kitchenware and punches of deep hues offer an updated take on rustic charm. Shop' },
+    { src: 'https://images.ctfassets.net/q602vtcuu3w3/1RZY1sC9SRpxYc7GWifbML/6452f71c115387bcf4036384e1ecc460/210301-WK1-HP-4.jpg?q=80&fm=webp', title: 'NEW FURNITURE: UPSTATE MODERN', text: 'Bright wood furniture, soft, modern textures, classic kitchenware and punches of deep hues offer an updated take on rustic charm. Merch and more, plus tons of vinyl, from new releases to reissues and special, limited edition UO Exclusives. Shop' }
+]
+export const mainGrid2 = [
     { src: 'https://images.ctfassets.net/q602vtcuu3w3/30Ied4M9BKbzjPrILTyYmf/f67dd59f9395b4a9519bdae1631e0757/210301-WK1-MGW-M-7.jpg?q=80&fm=webp', title: 'NEW DROP: OBEY CLOTHING', text: 'The latest apparel and accessories from the popular streetwear brand. Shop' },
-    { src: 'https://images.ctfassets.net/q602vtcuu3w3/2vLfTX2kgfwq3vBKR4Rgig/bfb3e9e4f37e7bcd7fc70ddcb95b74a3/210301-WK1-MGW-10.jpg?q=80&fm=webp', title: 'BACK IN STOCK', text: 'Music is everything, and it’s all right here. Audio gear, record players, merch and more, plus tons of vinyl, from new releases to reissues and special, limited edition UO Exclusives. Shop' },
-    { src: 'https://images.ctfassets.net/q602vtcuu3w3/3gAs0RuJlgvWbVuHNiC3wh/fb37cf36bec1c7a859817dbd5dfbb112/210301-WK1-MGW-11.jpg?q=80&fm=webp', title: 'KEDAI EXCLUSIVES', text: 'Music is everything, and it’s all right here. Audio gear, record players, merch and more, plus tons of vinyl, from new releases to reissues and special, limited edition UO Exclusives. Shop' },
-    { src: 'https://images.ctfassets.net/q602vtcuu3w3/4aTcU9lmXkXifLSduqbOto/b27f9d5d71ce2ac7b2c15dc3191c32ed/210301-WK1-HP-M-1.jpg?q=80&fm=webp', title: 'THE MUSIC SHOP', text: 'Music is everything, and it’s all right here. Audio gear, record players, merch and more, plus tons of vinyl, from new releases to reissues and special, limited edition UO Exclusives. Shop' },
+    { src: 'https://images.ctfassets.net/q602vtcuu3w3/2vLfTX2kgfwq3vBKR4Rgig/bfb3e9e4f37e7bcd7fc70ddcb95b74a3/210301-WK1-MGW-10.jpg?q=80&fm=webp', title: 'BACK IN STOCK', text: 'Music is everything, and it’s all right here. Audio gear, record players.' },
+    { src: 'https://images.ctfassets.net/q602vtcuu3w3/3gAs0RuJlgvWbVuHNiC3wh/fb37cf36bec1c7a859817dbd5dfbb112/210301-WK1-MGW-11.jpg?q=80&fm=webp', title: 'KEDAI EXCLUSIVES', text: 'Music is everything, and it’s all right here. Audio gear, record players.' },
+    { src: 'https://images.ctfassets.net/q602vtcuu3w3/4aTcU9lmXkXifLSduqbOto/b27f9d5d71ce2ac7b2c15dc3191c32ed/210301-WK1-HP-M-1.jpg?q=80&fm=webp', title: 'THE MUSIC SHOP', text: 'Music is everything, and it’s all right here. Audio gear, record players.' }
 ]
--- src/pages/Home/Grid.js
+++ src/pages/Home/Grid.js
@@ -1,24 +1,40 @@
 import React from 'react'
-import { mainGrid } from '../../components/data'
+import { mainGrid1, mainGrid2 } from '../../components/data'
 
 function Grid() {
     return (
         <div className='grid-container'>
-            {mainGrid.map((element, index) => {
-                const { src, title, text } = element;
-                { }
-                return (
-                    <div key={index} className={`element element${index}`}>
-                        <img src={src} alt={title} />
-                        { title && <div className="gridFooter">
-                            <h4>{title}</h4>
-                            <p>{text}</p>
+            <img class='main-grid-banner' src={mainGrid1[0].src} alt="banner" />
+            <section className="grid-left">
+                {mainGrid1.map((element, index) => {
+                    const { src, title, text } = element;
+                    return (
+                        <div key={index} className={`element element${index}`}>
+                            {title && <div className="gridFooter">
+                                <img src={src} alt={title} />
+                                <h4>{title}</h4>
+                                <p>{text}</p>
+                            </div>
+                            }
                         </div>
-                        }
-                    </div>
-                )
-            })}
-
+                    )
+                })}
+            </section>
+            <section className="grid-right">
+                {mainGrid2.map((element, index) => {
+                    const { src, title, text } = element;
+                    return (
+                        <div key={index} className={`element element${index + 5}`}>
+                            <img src={src} alt={title} />
+                            { title && <div className="gridFooter">
+                                <h4>{title}</h4>
+                                <p>{text}</p>
+                            </div>
+                            }
+                        </div>
+                    )
+                })}
+            </section>
         </div>
     )
 }
