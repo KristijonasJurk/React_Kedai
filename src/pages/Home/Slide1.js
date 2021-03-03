f94012c2d2e715c49fc5cbd5c1a69ef16a948235
KristijonasJurk <70882614+KristijonasJurk@users.noreply.github.com>
2021-03-03 22:50:42 +0200

    fixed slide functionality

--- src/pages/Home/Slide1.js
+++ src/pages/Home/Slide1.js
@@ -12,38 +12,40 @@ function Slide1() {
     const prevSlide = () => {
         console.log('prev');
         const width = containerWidth.current.getBoundingClientRect().width;
-        containerWidth.current.style.transform = `translateX(${width}px)`;
-        setUnactiveNext(true);
-        setUnctivePrev(false);
+        containerWidth.current.style.transform = `translateX(0px)`;
+        setUnactiveNext(false);
+        setUnctivePrev(true);
     }
     const nextSlide = () => {
         console.log('next');
         const width = containerWidth.current.getBoundingClientRect().width;
         containerWidth.current.style.transform = `translateX(-${width}px)`;
-        setUnactiveNext(false);
-        setUnctivePrev(true);
+        setUnactiveNext(true);
+        setUnctivePrev(false);
     }
 
     return (
         <div className='slide1-main'>
             <h5>New Arrivals</h5>
-            <div className="slide1-container" ref={containerWidth}>
-                {/* <div className="slide1-window"> */}
-                {slide1.map((element, index) => {
-                    const { src, text } = element;
-                    return (
-                        <article key={index} className='slide1-element'>
-                            <div className='slide1-image-container'>
-                                <img src={src} alt="" />
-                                <div className="slide1-image-footer">
-                                    <p>Quick Shop</p>
+            <div className="slide1-view">
+                <div className="slide1-container" ref={containerWidth}>
+                    {/* <div className="slide1-window"> */}
+                    {slide1.map((element, index) => {
+                        const { src, text } = element;
+                        return (
+                            <article key={index} className='slide1-element'>
+                                <div className='slide1-image-container'>
+                                    <img src={src} alt="" />
+                                    <div className="slide1-image-footer">
+                                        <p>Quick Shop</p>
+                                    </div>
                                 </div>
-                            </div>
-                            <p>{text}</p>
-                        </article>
-                    )
-                })}
-                {/* </div> */}
+                                <p>{text}</p>
+                            </article>
+                        )
+                    })}
+                    {/* </div> */}
+                </div>
                 <div className="slide1-buttons">
                     <button className='slide1-prev-button slide1-button' disabled={unactivePrev} onClick={prevSlide}>
                         <GrPrevious opacity={unactivePrev ? '0.4' : '1'} />
