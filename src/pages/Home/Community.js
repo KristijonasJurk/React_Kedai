e9f1e0ddd6df9737e7d7ddff27787cdb4b475ad2
KristijonasJurk <70882614+KristijonasJurk@users.noreply.github.com>
2021-03-04 16:01:23 +0200

    responsive design of community section

--- src/pages/Home/Community.js
+++ src/pages/Home/Community.js
@@ -4,7 +4,7 @@ import photo2 from '../../img/community/2.png';
 import photo3 from '../../img/community/3.png';
 
 const communityData = [
-    { id: 1, img: photo1, text: 'Turn your back to racism. Josh Pozzan the Black Lives Matter movement in detail' },
+    { id: 1, img: photo1, text: 'Turn your back to racism. Josh Pozzan the Black Lives Matter movement in detail.' },
     { id: 2, img: photo2, text: `Stacey Grammar: 'Dress to impress, with the latest Lindex dress!'` },
     { id: 3, img: photo3, text: `Raised in the streets: An inspiring story of a former NFL player. Life's never as easy as it seems...` }
 ]
@@ -18,7 +18,7 @@ function Community() {
                     <article key={id} className="community-element">
                         <img src={img} alt="community" />
                         <div className="community-footer">
-                            <h3>{text}</h3>
+                            <h4>{text}</h4>
                             <a href="#">
                                 Read More
                             </a>
