a48b82591cc76fe53fe1bd3c2123d40dcc7303f6
KristijonasJurk <70882614+KristijonasJurk@users.noreply.github.com>
2021-03-05 21:46:13 +0200

    added about section

--- src/pages/Home/About.js
+++ src/pages/Home/About.js
@@ -1,12 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { aboutData } from '../../components/data';
 
 function About() {
-    const [seeMore, setSeeMore] = useState(false);
+    const [seeMore, setSeeMore] = useState(true);
+    const text = seeMore ? aboutData.slice(0, 400) + '...' : aboutData;
+    useEffect(() => {
+        setSeeMore(true)
+    }, [])
+    const toggleSeeMore = () => {
+        setSeeMore(!seeMore)
+    }
     return (
         <div className='about-container'>
             <h3>About Kedai</h3>
-            <p>{aboutData}</p>
+            <p>{text}</p>
+            <span onClick={toggleSeeMore} className="about-show-btn">
+                {!seeMore ? 'See Less' : 'See More'}
+            </span>
         </div>
     )
 }
