9e2cab41d3cdb98416c8be38e6e9117d8374f5ab
KristijonasJurk <70882614+KristijonasJurk@users.noreply.github.com>
2021-03-08 02:31:18 +0200

    finished footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,11 @@
 import React, { useState, useLayoutEffect } from 'react'
-import { AiOutlineMail, AiOutlinePlus } from 'react-icons/ai';
-import { FaStoreAlt } from 'react-icons/fa';
+import { AiOutlineMail, AiOutlinePlus, AiFillTwitterCircle, AiFillGithub, AiFillAmazonCircle } from 'react-icons/ai';
+import { FaStoreAlt, FaFacebook, FaPinterest, FaSpotify } from 'react-icons/fa';
 
 const data = [
     { title: 'Help', links: ['Order status', 'Start A Return Or Exchange', 'Returns & Exchanges', 'Shipping', 'Order & Payments', 'FAQ', 'Product Recalls', 'Contact Us', 'See All Help Topics'] },
     { title: 'Quick Links', links: ['Brands', 'Gift Cards', 'Careers', 'Shop Europe', 'Shop N.America'] },
-    { title: 'Extras', links: ['Afterpay', 'Kedai Community', 'Creative Services', 'Diversity & Inclusion', 'Kedai Spaces'] },
+    { title: 'Extras', links: ['Afterpay', 'Kedai Community', 'Creative Services', 'Kedai MRKT', 'Charity', 'Diversity & Inclusion', 'Kedai Spaces'] },
     { title: ['Find Store', 'Get Email'] }
 ]
 
@@ -16,6 +16,10 @@ function Footer() {
     useLayoutEffect(() => {
         function updateSize() {
             setScreenWidth(window.innerWidth);
+            // when the screen is more than 800 width, the tabs are reset
+            if (window.innerWidth > 800) {
+                setShowLinks(-1)
+            }
         }
         window.addEventListener('resize', updateSize);
         updateSize();
@@ -47,6 +51,7 @@ function Footer() {
                                     <h5>{title}</h5>
                                     {screenWidth < 800 && <AiOutlinePlus className='footer-link-expand' onClick={() => setShowLinks(index)} />}
                                 </div>
+                                {/* screen size under 800 */}
                                 {showLinks === index && screenWidth < 800 ?
                                     <ul>
                                         {links.map((link) => {
@@ -59,6 +64,7 @@ function Footer() {
                                     </ul>
                                     : ''
                                 }
+                                {/* screen size over 800 */}
                                 {screenWidth > 800 ?
                                     <ul>
                                         {links.map((link) => {
@@ -88,7 +94,30 @@ function Footer() {
                     })}
                 </div>
                 <div className="footer-socials">
-                    footer socials template-rows 2/3
+                    <section className="footer-socials-download">
+                        <img src="https://images.ctfassets.net/q602vtcuu3w3/6dBQRtUvXacKeuqywuQgCs/9602843507b7084aff9e9bb9c4652459/Download_on_the_App_Store_Badge_EN_blk_092917-01.jpeg" alt="download on appstore" />
+                        <img src="https://images.ctfassets.net/q602vtcuu3w3/2mEsc8ts7uIasMuKaeEGoW/f0d28c0b2f7d54433597282a9c1b38b9/google-play-badge_EN-01.jpeg" alt="download on google" />
+                    </section>
+                    <section className="footer-socials-links">
+                        <FaFacebook color='rgb(58, 90, 152)' />
+                        <FaPinterest color='rgb(189, 9, 28)' />
+                        <FaSpotify color='rgb(35, 207, 95)' />
+                        <AiFillAmazonCircle />
+                        <AiFillTwitterCircle color='rgb(0, 172, 237)' />
+                        <AiFillGithub />
+                    </section>
+                    <section className="footer-socials-docs">
+                        <ul>
+                            <li>Privacy Policy</li>
+                            <li>Terms of Use </li>
+                            <li>CA Transparency</li>
+                            <li>Accessibility</li>
+                            <li>Membership</li>
+                        </ul>
+                    </section>
+                    <section className="footer-socials-rights">
+                        <p>© 2021 Kristijonas Jurkšas All Rights Reserved</p>
+                    </section>
                 </div>
             </section>
         </footer>
