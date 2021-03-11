4088462bd88a21b9a6ecfb28c9cd8f664d630343
KristijonasJurk <70882614+KristijonasJurk@users.noreply.github.com>
2021-03-12 00:47:58 +0200

    adding womens scss

--- src/pages/Womens/Womens.js
+++ src/pages/Womens/Womens.js
@@ -1,19 +1,26 @@
-import React from 'react'
+import React from 'react';
+import { womensBrowse } from '../../components/data';
+import { useGlobalContext } from '../../context'
 
 function Womens() {
+    const { closeSubmenu } = useGlobalContext();
     return (
-        <div className='womens-container'>
+        <div className='womens-container' onMouseOver={closeSubmenu}>
             <section className="womens-filter-container">
-                <b>Brwose by:</b>
+                <p>Browse by:</p>
                 <div className="womens-filters">
                     <ul>
-                        {/* DATAAAAA */}
+                        {womensBrowse.map((category, index) => {
+                            return (
+                                <li><a href="#">{category}</a></li>
+                            )
+                        })}
                     </ul>
                 </div>
             </section>
             <section className="womens-products-container">
-                <header>
-                    <h3>Women's clothing <span>4864 results</span></h3>
+                <header className='womens-products-header'>
+                    <h3>Women's Clothing <span>4864 results</span></h3>
                     <div className="womens-sort-container">
                         <label htmlFor="womens-sort">Sort:</label>
                         <select name="womens sort" id="womens-sort">
@@ -24,17 +31,23 @@ function Womens() {
                             <option value="Z-A">Z-A</option>
                         </select>
                     </div>
-                    <div className="womens-products-container">
-                        <ul>
-                            <li>
-                                <img src={`https://s7d5.scene7.com/is/image/UrbanOutfitters/59155721_010_d?$medium$&qlt=80&fit=constrain`} alt="shirt" />
-                                <div className="womens-product-footer">
-
-                                </div>
-                            </li>
-                        </ul>
-                    </div>
                 </header>
+                <div className="womens-products-container">
+                    <ul>
+                        <li>
+                            <img src={`https://s7d5.scene7.com/is/image/UrbanOutfitters/59155721_010_d?$medium$&qlt=80&fit=constrain`} alt="shirt" />
+                            <div className="womens-product-footer">
+                                <p>Project Social T Rose Infused Oversized Tee</p>
+                                <p>$39.00</p>
+                                <div className="product-colors">
+                                    <div className="product-color"></div>
+                                    <div className="product-color"></div>
+                                </div>
+                                <span>Back in Stock</span>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
             </section>
         </div>
     )
