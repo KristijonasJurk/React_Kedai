a20ef7bfe9b17f1d2f18ba760e61963a6509330a
KristijonasJurk <70882614+KristijonasJurk@users.noreply.github.com>
2021-03-11 13:05:00 +0200

    adding womens.js

--- src/pages/Womens/Womens.js
+++ src/pages/Womens/Womens.js
@@ -2,8 +2,40 @@ import React from 'react'
 
 function Womens() {
     return (
-        <div>
-            heloooooooooo
+        <div className='womens-container'>
+            <section className="womens-filter-container">
+                <b>Brwose by:</b>
+                <div className="womens-filters">
+                    <ul>
+                        {/* DATAAAAA */}
+                    </ul>
+                </div>
+            </section>
+            <section className="womens-products-container">
+                <header>
+                    <h3>Women's clothing <span>4864 results</span></h3>
+                    <div className="womens-sort-container">
+                        <label htmlFor="womens-sort">Sort:</label>
+                        <select name="womens sort" id="womens-sort">
+                            <option hidden value="Featured">Featured</option>
+                            <option value="Low-to-High">Low to High</option>
+                            <option value="High-to-Low">High to Low</option>
+                            <option value="A-Z">A-Z</option>
+                            <option value="Z-A">Z-A</option>
+                        </select>
+                    </div>
+                    <div className="womens-products-container">
+                        <ul>
+                            <li>
+                                <img src={`https://s7d5.scene7.com/is/image/UrbanOutfitters/59155721_010_d?$medium$&qlt=80&fit=constrain`} alt="shirt" />
+                                <div className="womens-product-footer">
+
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </header>
+            </section>
         </div>
     )
 }
