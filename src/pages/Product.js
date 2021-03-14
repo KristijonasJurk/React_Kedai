598faa2b214072733a9c337dcc5b43467489b415
KristijonasJurk <70882614+KristijonasJurk@users.noreply.github.com>
2021-03-14 17:00:18 +0200

    adding single products js

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -2,13 +2,17 @@ import React, { useState, useEffect } from 'react'
 import { womensBrowse, womensProducts } from '../components/data';
 import { Link, useParams } from 'react-router-dom';
 
+const shoeSizes = [38, 39, 40, 41, 42, 43, 44, 45];
+const clothesSizes = ['XS', 'S', 'M', 'L', 'XL']
+
 function Product() {
     const { id } = useParams();
-    const [data, setData] = useState(null);
+    const [data,] = useState(womensProducts.find((product) => product.id === parseInt(id)));
+
+    const [secondColor, setSecondColor] = useState(false)
 
     useEffect(() => {
-        const specificProduct = womensProducts.find((product) => product.id === parseInt(id));
-        setData(specificProduct)
+
     }, [])
 
     return (
@@ -28,12 +32,63 @@ function Product() {
                     </div>
                 </section>
                 <section className="product-info-container">
-                    <h2>{data.title}</h2>
-                    <h2>€{data.price}</h2>
-                    <div className="product-colors-container">
-                        <p>Colors <span>{data.color}</span></p>
-
-                    </div>
+                    <section className="product-info-main">
+                        <h2>{data.title}</h2>
+                        <h2>€{data.price}</h2>
+                        <div className="product-colors-container">
+                            <p className='product-bold-text'>Color: {data.color.map((clr, index) =>
+                            // if the color is an array, it renders multiple
+                            {
+                                return (
+                                    <span>{clr}</span>
+                                )
+                            })}</p>
+                            <div style={data.img2 ? { backgroundColor: data.color[0] } : { backgroundColor: data.color }} className="product-color product-color2" onClick={() => setSecondColor(false)}></div>
+                            {data.img2 && <div style={{ backgroundColor: data.color[1] }} className="product-color product-color2" onClick={() => setSecondColor(true)}></div>}
+                        </div>
+                        <div className="product-size-container">
+                            <p className='product-bold-text'>Size*</p>
+                            <div className="product-sizes">
+                                {data.categories.includes("shoes") ?
+                                    // for shoes display shoe sizes, and for clothes clothe sizes
+                                    shoeSizes.map((size, i) => {
+                                        return (
+                                            <div>{size}</div>
+                                        )
+                                    })
+                                    :
+                                    clothesSizes.map((size, i) => {
+                                        return (
+                                            <div>{size}</div>
+                                        )
+                                    })
+                                }
+                            </div>
+                        </div>
+                        <div className="quantity-container">
+                            <p className='product-bold-text'>Qty*</p>
+                            <select name="product sort" id="product-sort">
+                                <option value="1units">1</option>
+                                <option value="2units">2</option>
+                                <option value="3units">3</option>
+                                <option value="4units">4</option>
+                                <option value="5units">5</option>
+                                <option value="6units">6</option>
+                                <option value="7units">7</option>
+                                <option value="8units">8</option>
+                                <option value="9units">9</option>
+                                <option value="10units">10</option>
+                            </select>
+                        </div>
+                        <button className="product-add-to-cart-btn">
+                            Add to Cart
+                    </button>
+                    </section>
+                    <section className="product-info-details">
+                        <h4>Details</h4>
+                        <p>{data.text}</p>
+                        <b>Material</b>
+                    </section>
                 </section>
             </div>
         </div>
